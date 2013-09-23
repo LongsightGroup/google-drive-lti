bd1a5ab3b4fa0bab07a4a3649797a725b4db59ed
Gonzalo Silverio <gsilver@umich.edu>
2013-09-23 17:24:11 +0000

    NOJIRA - add screen reader help text for expand and collapse actions

--- src/main/webapps/js/googleDriveLti.js
+++ src/main/webapps/js/googleDriveLti.js
@@ -49,8 +49,8 @@ var FILE_DEPTH_PADDING_PX = 20;
 // all a file's ancestors can be done by finding file's parent, then finding
 // entry for each parent's parent.
 var googleFileParents = [];
-var EXPAND_TEXT = '+';
-var SHRINK_TEXT = '-';
+var EXPAND_TEXT = '+ <span class="hide-text">Expand this folder</span>';
+var SHRINK_TEXT = '- <span class="hide-text">Collapse this folder</span>';
 
 var accessTokenHandler = {
 		"accessToken" : null,
