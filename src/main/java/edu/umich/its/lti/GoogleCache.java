0cfe7eacc2240a2ef5fe4bcc7392c390fa7eb517
Pushyami Gundala <pushyami@umich.edu>
2013-11-12 19:31:53 +0000

    CodeReviewChange(CRC)-Utilized concurrentHashMap instead of HashMap for thread safty.

--- src/main/java/edu/umich/its/lti/GoogleCache.java
+++ src/main/java/edu/umich/its/lti/GoogleCache.java
@@ -1,7 +1,13 @@
 package edu.umich.its.lti;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * This class is used to store the unlinked folder in a map 
+ * just before the deletion from the LTI setting service 
+ * and used later during the deletion of permissions in google drive
+ * **/
 
 public class GoogleCache {
 	private static final GoogleCache INSTANCE=new GoogleCache();
@@ -10,7 +16,8 @@ public class GoogleCache {
 		return INSTANCE;
 	}
 	
-	private Map<String, TcSiteToGoogleLink> siteI2LinkMap=new HashMap<String, TcSiteToGoogleLink>();
+	//used Concurrent Map for thread safety
+	private Map<String, TcSiteToGoogleLink> siteI2LinkMap =new ConcurrentHashMap<String,TcSiteToGoogleLink>(); 
 	
 	private GoogleCache() {
 		
